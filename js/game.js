eedfd430cc62ff08715f87eff47e13191037f666
Pat <27511950+ThePat02@users.noreply.github.com>
2019-03-03 17:37:21 +0100

    dumb change

--- js/game.js
+++ js/game.js
@@ -26,7 +26,7 @@ function nextYear()
 
 function updateSchoolInfo()
 {
-  if (m_age > 5) m_schoolyear = m_school + 1;
+  if (m_age > 5) m_schoolyear = m_schoolyear + 1;
   if (m_schoolyear < 13) {
     changeContent("info_schoolyear", "Year " + m_schoolyear);
     changeContent("info_grades", calcGrades());
