26ff4b9d2afe1e6544ecfc30152e9c728f1aa5f6
Pat <27511950+ThePat02@users.noreply.github.com>
2019-03-03 18:00:17 +0100

    Removed scaling

--- js/event.js
+++ js/event.js
@@ -96,8 +96,6 @@ function chooseSchool(school_arg) {
 
 function calcGrades() {
   var grade = m_intellect + (m_fitness / 3) + generateRandom(1, 30);
-  m_intellect = m_intellect + grade/15;
-  m_popularity = m_popularity + grade/20;
 
   var result = "";
 
