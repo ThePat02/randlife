05a649b4091057d51241b4313bfb915847fdc106
Pat <27511950+ThePat02@users.noreply.github.com>
2019-03-03 19:12:52 +0100

    scaling

--- js/event-scripts/school-events.js
+++ js/event-scripts/school-events.js
@@ -2,7 +2,7 @@ function studentCouncil()
 {
   if (m_intellect + m_popularity > 99) {
     write("You are elected to join the student council.");
-    m_popularity = m_popularity + 3;
+    m_popularity = m_popularity + 10;
   }
   else {
     write("You are not smart or popular enough to join the student council.");
